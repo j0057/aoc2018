43e104cda65c28d9fb0971b051d68607446cd09d
Joost Molenaar <j.j.molenaar@gmail.com>
2018-12-09 20:35:02 +0100

    [01b] Chronal Calibration: part two

--- rust/src/day01.rs
+++ rust/src/day01.rs
@@ -1,5 +1,22 @@
+use std::collections::HashSet;
+
 pub fn day01a(m: &[i64]) -> i64 {
     m.iter().sum()
+}
+
+pub fn day01b(deltas: &[i64]) -> i64 {
+    let mut seen = HashSet::new();
+    let mut freq: i64 = 0;
+    seen.insert(0);
+    loop {
+        for delta in deltas.iter() {
+            freq += delta;
+            if seen.contains(&freq) {
+                return freq;
+            }
+            seen.insert(freq);
+        }
+    }
 }
 
 pub mod util {
@@ -46,6 +63,39 @@ mod test {
         let n = super::util::get_numbers("../input/day01.txt")?;
         let r = super::day01a(&n);
         assert_eq!(r, 508);
+        Ok(())
+    }
+
+    #[test]
+    fn test01b0() {
+        assert_eq!(super::day01b(&[1, -2, 3, 1]), 2);
+    }
+
+    #[test]
+    fn test01b1() {
+        assert_eq!(super::day01b(&[1, -1]), 0);
+    }
+
+    #[test]
+    fn test01b2() {
+        assert_eq!(super::day01b(&[3, 3, 4, -2, -4]), 10);
+    }
+
+    #[test]
+    fn test01b3() {
+        assert_eq!(super::day01b(&[-6, 3, 8, 5, -6]), 5);
+    }
+
+    #[test]
+    fn test01b4() {
+        assert_eq!(super::day01b(&[7, 7, -2, -7, -4]), 14);
+    }
+
+    #[test]
+    fn test01b() -> std::io::Result<()> {
+        let n = super::util::get_numbers("../input/day01.txt")?;
+        let r = super::day01b(&n);
+        assert_eq!(r, 549);
         Ok(())
     }
 }
