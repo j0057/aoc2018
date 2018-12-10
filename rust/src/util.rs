b649c9b6ed61a584f74d43a924d184048c038e1e
Joost Molenaar <j.j.molenaar@gmail.com>
2018-12-10 05:40:10 +0100

    Extract function for reading input as a Vec<String>

--- rust/src/util.rs
+++ rust/src/util.rs
@@ -2,11 +2,14 @@ use std::fs::File;
 use std::io;
 use std::io::BufRead;
 
-pub fn get_numbers(filename: &str) -> io::Result<Vec<i64>> {
+pub fn get_lines(filename: &str) -> io::Result<Vec<String>> {
     let file = File::open(filename)?;
     let reader = io::BufReader::new(file);
-    let lines = reader.lines().collect::<io::Result<Vec<String>>>()?;
-    lines
+    return reader.lines().collect::<io::Result<Vec<String>>>();
+}
+
+pub fn get_numbers(filename: &str) -> io::Result<Vec<i64>> {
+    get_lines(filename)?
         .iter()
         .map(|s| s.parse::<i64>())
         .collect::<Result<Vec<i64>, std::num::ParseIntError>>()
