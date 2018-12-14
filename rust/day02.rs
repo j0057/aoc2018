e557fecd1a70076f17f087dadce98f8494982228
Joost Molenaar <j.j.molenaar@gmail.com>
2018-12-14 05:28:13 +0100

    day02.rs

--- rust/day02.rs
+++ rust/day02.rs
@@ -1,4 +1,14 @@
+use std::collections::HashMap;
 
-pub fn day02a(ids: &[String]) -> String {
-    ids[0].clone()
+fn letter_count(word: &String) -> HashMap<char, u64> {
+    let mut letters = HashMap::<char, u64>::new();
+    for ch in word.chars() {
+        *letters.entry(ch).or_insert(0) += 1;
+    }
+    return letters;
+}
+
+pub fn day02a(ids: &[String]) -> u64 {
+    let counts = ids.iter().map(letter_count).collect::<Vec<_>>();
+    return counts.iter().filter(|c| c.values().any(|&v| v == 2)).count();
 }
