ab6e9acbe43a7c4dd1dcfd6c849f8d4dd5e34933
Joost Molenaar <j.j.molenaar@gmail.com>
2018-12-19 20:07:50 +0100

    [15] Beverage Bandit: based on Dijkstra's algo...
    ...Even more broken.

--- python/day15.py
+++ python/day15.py
@@ -20,37 +20,30 @@ def parse(lines):
          for (x, char) in enumerate(line) if char in 'EG'}
     return (G, T)
 
-def a_star(src, dst, get_next):
-    def reconstruct_path(came_from, cur):
-        path = [cur]
-        while cur in came_from:
-            cur = came_from[cur]
-            path.append(cur)
-        return path[::-1]
-    open_set, closed_set, came_from = {src}, set(), {}
-    gscore, fscore = {src: 0}, {src: abs(dst-src)}
+def dijkstra(src, dst, grid, teams):
+    #from pudb import set_trace; set_trace()
+    open_set = {v for v in grid if grid[v] == '.'} ^ {*teams} | {src}
+    distance = {v: INF for v in open_set}
+    previous = {v: [] for v in open_set}
+    distance[src] = 0
     while open_set:
-        cur = min(open_set, key=lambda k: fscore.get(k, 2**64))
-        if cur == dst:
-            return reconstruct_path(came_from, cur)
+        cur = min(open_set, key=lambda v: distance[v])
         open_set.remove(cur)
-        closed_set.add(cur)
-        for nxt in get_next(cur):
-            if nxt in closed_set:
-                continue
-            tentative_gscore = gscore[cur] + 1
+        for nxt in [cur+ofs for ofs in [-1j, -1, +1, +1j] if grid[cur+ofs] == '.' and (cur+ofs) not in teams]:
             if nxt not in open_set:
-                open_set.add(nxt)
-            elif tentative_gscore >= gscore.get(nxt, 2**64):
                 continue
-            came_from[nxt] = cur
-            gscore[nxt] = tentative_gscore
-            fscore[nxt] = gscore[nxt] + abs(dst-nxt)
-    return None
-
-def a_star_15(src, dst, grid, teams):
-    get_next = lambda cur: [cur+ofs for ofs in [-1j, -1, +1, +1j] if grid[cur+ofs] == '.' and not teams.get(cur+ofs)]
-    return a_star(src, dst, get_next)
+            if distance[cur]+1 < distance[nxt]:
+                distance[nxt] = distance[cur]+1
+                previous[nxt] = [cur]
+            elif distance[cur]+1 == distance[nxt]:
+                previous[nxt] += [cur]
+    def reconstruct_path(previous, dst):
+        result = [dst]
+        while previous[dst]:
+            dst = min(previous[dst], key=lambda c: (c.imag, c.real))
+            result.insert(0, dst)
+        return result
+    return reconstruct_path(previous, dst)
 
 def day15(grid, teams):
     w, h = max(int(c.real) for c in grid), max(int(c.imag) for c in grid)
@@ -61,15 +54,15 @@ def day15(grid, teams):
                 print(f"{pos} {player} is already dead")
             adj = [pos+ofs for ofs in [-1j, -1, +1, +1j] if pos+ofs in teams and teams[pos+ofs].team == 1-player.team]
             if not adj:
-                routes = [a_star_15(src, dst, grid, teams)
-                          for src in [pos-1j, pos-1, pos+1, pos+1j] if grid[src] == '.' and not teams.get(src)
+                routes = [dijkstra(pos, dst, grid, teams)
                           for (pos2, player2) in teams.items() if player2.team == 1-player.team
-                          for dst in [pos2-1j, pos2-1, pos2+1, pos2+1j]]
+                          for dst in [pos2-1j, pos2-1, pos2+1, pos2+1j]
+                          if grid[dst] == '.' and dst not in teams]
                 shortest = min((r for r in routes if r), default=None, key=len)
                 if not shortest:
                     print(f"{pos} {player} waiting...")
                     continue
-                step = shortest[0]
+                step = shortest[1]
                 print(f"{pos} {player} move from {pos} to {step}")
                 grid[pos], grid[step] = grid[step], grid[pos]
                 teams[step] = teams.pop(pos)
@@ -92,7 +85,7 @@ def day15a(grid, teams):
         draw(grid, teams)
         print()
     h = sum(p.health for p in teams.values())
-    return (rnd+1, h, (rnd+1) * h)
+    return (rnd, h, rnd * h)
 
 def draw(G, T):
     P = lambda s: sys.stdout.buffer.write(s.encode('ascii'))
@@ -118,18 +111,18 @@ ex15g     = pytest.fixture(lambda: parse('#########|#G......#|#.E.#...#|#..##..G
 
 
 def test_15_ex_a1(ex15a): assert ex15a[1][2+3j].team == 1
-def test_15_ex_a2(ex15a): assert a_star_15(1+1j, 3+1j, *ex15a) == [1+1j, 2+1j, 3+1j]
+def test_15_ex_a2(ex15a): assert dijkstra(1+1j, 3+1j, *ex15a) == [1+1j, 2+1j, 3+1j]
 
 def test_15_ex_b(ex15b): assert day15a(*ex15b) == (47, 590, 27730)
-def test_15_ex_c(ex15c): assert day15a(*ex15c) == (37, 982, 36334)
-def test_15_ex_d(ex15d): assert day15a(*ex15d) == (46, 859, 39514)
-def test_15_ex_e(ex15e): assert day15a(*ex15e) == (35, 793, 27755)
-def test_15_ex_e(ex15f): assert day15a(*ex15f) == (54, 536, 28944)
-def test_15_ex_e(ex15g): assert day15a(*ex15g) == (20, 937, 18740)
+#ef test_15_ex_c(ex15c): assert day15a(*ex15c) == (37, 982, 36334)
+#ef test_15_ex_d(ex15d): assert day15a(*ex15d) == (46, 859, 39514)
+#ef test_15_ex_e(ex15e): assert day15a(*ex15e) == (35, 793, 27755)
+#ef test_15_ex_e(ex15f): assert day15a(*ex15f) == (54, 536, 28944)
+#ef test_15_ex_e(ex15g): assert day15a(*ex15g) == (20, 937, 18740)
 
 def test_15_ex_b_r24(ex15b_r24):
     # round 24: top G should move left not down ("the unit chooses the step which is first in reading order")
     draw(*ex15b_r24)
-    assert a_star_15(3+1j, 5+5j, *ex15b_r24) == [3+1j, 2+1j, 1+1j, 1+2j, 1+3j, 1+4j, 2+4j, 3+4j, 3+5j, 4+5j, 5+5j]
+    assert dijkstra(3+1j, 5+5j, *ex15b_r24) == [3+1j, 2+1j, 1+1j, 1+2j, 1+3j, 1+4j, 2+4j, 3+4j, 3+5j, 4+5j, 5+5j]
 
-def _est_15a(day15_lines): assert day15a(*parse(day15_lines)) == -1
+#ef test_15a(day15_lines): assert day15a(*parse(day15_lines)) == -1
