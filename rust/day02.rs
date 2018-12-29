0038b818783dced72ed422873856cbb89e2bd752
Joost Molenaar <j.j.molenaar@gmail.com>
2018-12-29 23:45:47 +0100

    Pass String/&str as AsRef<T>
    This took at 10 days to figure out...

--- rust/day02.rs
+++ rust/day02.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-fn letter_count(word: &String) -> HashMap<char, u64> {
+fn letter_count(word: &str) -> HashMap<char, u64> {
     let mut letters = HashMap::<char, u64>::new();
     for ch in word.chars() {
         *letters.entry(ch).or_insert(0) += 1;
@@ -8,12 +8,11 @@ fn letter_count(word: &String) -> HashMap<char, u64> {
     return letters;
 }
 
-pub fn day02a(ids: &[String]) -> u64 {
-    let counts = ids.iter().map(letter_count).collect::<Vec<_>>();
+pub fn day02a<T: AsRef<str>>(ids: &[T]) -> u64 {
+    let counts = ids.iter().map(|s| letter_count(s.as_ref())).collect::<Vec<_>>();
     return counts.iter().filter(|c| c.values().any(|&v| v == 2)).count() as u64
          * counts.iter().filter(|c| c.values().any(|&v| v == 3)).count() as u64;
 }
-
 
 #[cfg(test)]
 mod test {
@@ -23,15 +22,7 @@ mod test {
 
     #[test]
     fn test02a1() {
-        assert_eq!(super::day02a(&[
-            String::from("abcdef"),
-            String::from("bababc"),
-            String::from("abbcde"),
-            String::from("abcccd"),
-            String::from("aabcdd"),
-            String::from("abcdee"),
-            String::from("ababab"),
-        ]), 12);
+        assert_eq!(super::day02a(&["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"]), 12);
     }
 
     #[test]
