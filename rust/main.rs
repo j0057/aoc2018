fc2211ff6105ae9a77352f0b2fab5ad831942985
Joost Molenaar <j.j.molenaar@gmail.com>
2018-12-31 20:20:28 +0100

    Take I/O out of stopwatch

--- rust/main.rs
+++ rust/main.rs
@@ -5,31 +5,26 @@ mod day02;
 use std::error::Error;
 use std::time;
 
-type Solution = fn() -> Result<String, Box<Error>>;
+fn get_puzzles() -> Result<Vec<(u8, char, Box<Fn() -> String>)>, Box<Error>> {
+    let day01a = util::get_numbers("input/day01.txt")?;
+    let day01b = day01a.clone();
 
-fn get_puzzles() -> Vec<(u8, char, Solution)> {
-    vec![
-        (1, 'A', || {
-            let day01 = util::get_numbers("input/day01.txt")?;
-            Ok(day01::day01a(&day01).to_string())
-        }),
-        (1, 'B', || {
-            let day01 = util::get_numbers("input/day01.txt")?;
-            Ok(day01::day01b(&day01).to_string())
-        }),
-        (2, 'A', || {
-            let day02 = util::get_lines("input/day02.txt")?;
-            Ok(day02::day02a(&day02).to_string())
-        }),
-    ]
+    let day02a = util::get_lines("input/day02.txt")?;
+
+    let result: Vec<(u8, char, Box<Fn() -> String>)> = vec![
+        (1, 'A', Box::new(move || day01::day01a(&day01a).to_string())),
+        (1, 'B', Box::new(move || day01::day01b(&day01b).to_string())),
+        (2, 'A', Box::new(move || day02::day02a(&day02a).to_string())),
+    ];
+    Ok(result)
 }
 
 fn main() -> Result<(), Box<Error>> {
-    let puzzles = get_puzzles();
+    let puzzles = get_puzzles()?;
 
     for (problem, part, solution) in puzzles.iter() {
         let start = time::SystemTime::now();
-        let answer = solution()?;
+        let answer = solution();
         let elapsed = start.elapsed()?;
         let micros = elapsed.as_secs() * 1_000_000 + (elapsed.subsec_micros() as u64);
         println!("problem: {:>2}{}; time: {:9} μs, answer: {}", problem, part, micros, answer);
